e15d9b22fd777b2b12f07b4ecb10b8b6e3e00fc7
vinirrai <111763458+vinirrai@users.noreply.github.com>
2025-03-21 15:24:55 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Hello, Let's talk Law`;
+export const MESSAGE_PLACEHOLDER: string = `How can I help you?`;
 export const FOOTER_MESSAGE: string = `Customized AI by Vinir R @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
+export const PAGE_TITLE: string = `LegalMate AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
