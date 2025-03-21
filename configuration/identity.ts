f42ffe933e5873f1a1454f1fdbe7d857e34fa34d
vinirrai <111763458+vinirrai@users.noreply.github.com>
2025-03-21 15:33:04 -0400

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Vinir`;
 export const OWNER_DESCRIPTION: string = `A tech enthusiast currently in college, helping materlize new ideas in the field.`;
 
-export const AI_NAME: string = `LegalMate- A lawyer for your startup`;
+export const AI_NAME: string = `LegalMate - Your Startup Legal Assistant`;
 export const AI_TONE: string = `Logical`;
-export const AI_ROLE: string = `A chatbot that analyzes and summarizes complex legal documents for startup founders, making legal paperwork easier to understand and navigate.`;
+export const AI_ROLE: string = `An AI assistant that simplifies and explains legal documents for startup founders, helping them navigate business law with ease.`;
