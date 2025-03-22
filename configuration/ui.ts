af0fd1460fa07ac54a01358393bd01aad3f22b82
vinirrai <111763458+vinirrai@users.noreply.github.com>
2025-03-21 20:19:51 -0400

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Hello, Let's talk Law`;
+export const CHAT_HEADER: string = `Hello, Let's talk about start-up business law!`;
 export const MESSAGE_PLACEHOLDER: string = `How can I help you?`;
 export const FOOTER_MESSAGE: string = `Customized AI by Vinir R @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
